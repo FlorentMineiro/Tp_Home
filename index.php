dbae50f664076bb79e8023e19db6edd94fc6a2dd
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-10-23 00:07:43 +0200

    Nouveau commit

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ use poo\Cabane as Cabane;
 
 //Liste Batiments;
 $batiment = array();
+/*$batiment[0] = new Batiment('Pierre Maison',250,'Maison Individuelle',3);
+$batiment[1] = new Batiment('Solaris',950,"Immeuble Résidentiel",6);
+$batiment[2]= new Batiment('Nova',1300,'Immeuble Mixte',8);
+$batiment[3]=new Batiment('Gardien Maison',95,'Maison Fonctionnelle',1);*/
 
 
 //Liste à ajouter au premier batiment
@@ -17,7 +21,7 @@ $cabane1 = new Cabane("Cabane de jardin de Pierre",5,"Cabane De Jardin",1);
 $cabane2 = new Cabane("Cabane perchée de Pierre",5,"Cabane De jeu pour enfants",1);
 $cabane3 = new Cabane("Cabane de travail de Pierre",15,"Tiny House",2);
 
-$batiment[0]=[$maison1
+$batiment[1]=[$maison1
             ,$cabane1
             ,$cabane2
             ,$cabane3];
@@ -31,7 +35,7 @@ $cabane4 = new Cabane("Atelier de peinture suspendu", 7, "Cabane perchée artist
 $cabane5 = new Cabane("Serre urbaine", 10, "Cabane de jardin écologique", 1);
 $cabane6 = new Cabane("Studio d’écriture minimaliste", 20, "Tiny House créative", 2);
 
-$batiment[1] = [$immeuble1, $cabane4, $cabane5, $cabane6];
+$batiment[2] = [$immeuble1, $cabane4, $cabane5, $cabane6];
 
 //Liste à ajouter au troisième batiment;
 $immeuble2 = new Immeuble("Tour Nova", 1300, "Immeuble Mixte", 8, true, 40);
@@ -42,7 +46,7 @@ $cabane7 = new Cabane("Poste de sécurité modulaire", 9, "Cabane technique", 1)
 $cabane8 = new Cabane("Espace zen flottant", 12, "Cabane de repos sur pilotis", 1);
 $cabane9 = new Cabane("Bureau nomade", 22, "Tiny House mobile", 2);
 
-$batiment[2] = [$immeuble2, $cabane7, $cabane8, $cabane9];
+$batiment[3] = [$immeuble2, $cabane7, $cabane8, $cabane9];
 
 //Liste à ajouter au quatrième batiment;
 
@@ -51,24 +55,44 @@ $immeuble3 = new Immeuble("Tour NovaTech", 1400, "Immeuble Tertiaire", 10, true,
 $immeuble3->setGarage(false);
 $cabane10 = new Cabane("Cabane serveur", 15, "Cabane technique", 1);
 
-$batiment[3] = [$maison2, $immeuble3, $cabane10];
+$batiment[4] = [$maison2, $immeuble3, $cabane10];
 
-foreach($batiment as $indexBatiment)
+echo "<h1>"."LISTE DES BATIMENTS"."</h1>"."<br>";
+$id = isset($_GET['id'])?(int)$_GET['id']:0;
+
+
+
+if($id && isset($_GET['id']))
 {
-    foreach($indexBatiment as $indexTypeBatiment)
-    {
-        if ($indexTypeBatiment instanceof Maison)
+    $batimentSelectionne = $batiment[$id];
+}else
+{
+    $batimentSelectionne = null;
+}
+foreach($batiment as $indexDuBatiment => $batimentGroupe)
+{
+    $nomAffiche = $batimentGroupe[0]->getNomBatiment();
+   echo '<a href="index.php?id='.$indexDuBatiment.'">'.$nomAffiche.'</a><br><br>';
+}
+if ($batimentSelectionne)
+{
+    foreach($batimentSelectionne as $indexBatiment)
+{
+    
+        if ($indexBatiment instanceof Maison)
         {
-                echo "Nom Maison : ".$indexTypeBatiment->getNomBatiment()." - ".$indexTypeBatiment->getSuperficieBatiment()."m² - ".$indexTypeBatiment->getTypeBatiment()." - ".$indexTypeBatiment->getNbEtageBatiment()." - ".($indexTypeBatiment->getCheminee()?'true':'false')." - ".($indexTypeBatiment->getGarageMaison()?'true':'false')."\n";
-        }elseif($indexTypeBatiment instanceof Immeuble)
+                echo " <strong>Maison :</strong> ".$indexBatiment->getNomBatiment()." - ".$indexBatiment->getSuperficieBatiment()."m² - ".$indexBatiment->getTypeBatiment()." - ".$indexBatiment->getNbEtageBatiment()."étage(s) - ".($indexBatiment->getCheminee()?'true':'false')." - ".($indexBatiment->getGarageMaison()?'true':'false')."<br>";
+        }elseif($indexBatiment instanceof Immeuble)
         {
-                 echo "Nom Immeuble : ".$indexTypeBatiment->getNomBatiment()." - ".$indexTypeBatiment->getSuperficieBatiment()."m² - ".$indexTypeBatiment->getTypeBatiment()." - ".$indexTypeBatiment->getNbEtageBatiment()." - ".($indexTypeBatiment->getAscenseur()?'true':'false')." - ".$indexTypeBatiment->getNbAppatement()." - ".($indexTypeBatiment->getGarage()?'true':'false')."\n";
-        }elseif($indexTypeBatiment instanceof Cabane)
+                 echo "<strong> Immeuble</strong> : ".$indexBatiment->getNomBatiment()." - ".$indexBatiment->getSuperficieBatiment()."m² - ".$indexBatiment->getTypeBatiment()." - ".$indexBatiment->getNbEtageBatiment()."étage(s) - ".($indexBatiment->getAscenseur()?'true':'false')." - ".$indexBatiment->getNbAppatement()."appartement(s) - ".($indexBatiment->getGarage()?'true':'false')."<br>";
+        }elseif($indexBatiment instanceof Cabane)
         {
-                 echo "Nom Cabane : ".$indexTypeBatiment->getNomBatiment()." - ".$indexTypeBatiment->getSuperficieBatiment()."m² - ".$indexTypeBatiment->getTypeBatiment()." - ".$indexTypeBatiment->getNbEtageBatiment()."\n";
+                 echo " <strong>Cabane</strong> : ".$indexBatiment->getNomBatiment()." - ".$indexBatiment->getSuperficieBatiment()."m² - ".$indexBatiment->getTypeBatiment()." - ".$indexBatiment->getNbEtageBatiment()."étage(s) "."<br>";
         }
         
-    }
+    
 }
+}
+
 
 
